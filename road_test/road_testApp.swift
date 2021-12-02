69830594ae5601a12430d51a003609aa6d3b9f89
lcwd1119 <71938251+lcwd1119@users.noreply.github.com>
2021-12-02 11:16:46 +0800

    Initial Commit

---
+++ road_test/road_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  road_testApp.swift
+//  road_test
+//
+//  Created by 廖晨維 on 2021/12/2.
+//
+
+import SwiftUI
+
+@main
+struct road_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
